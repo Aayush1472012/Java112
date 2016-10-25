ea458df88c5b5aac4241c8b12d1d8e93c5d2073b
pcampbelldawson <pcampbell.dawson@gmail.com>
2016-10-25 20:14:03 +0000

    fix syntax loop1 snippet

--- lectures/lecture19/snippet-loop1.java
+++ lectures/lecture19/snippet-loop1.java
@@ -1,6 +1,9 @@
 /**
  * this method is overloaded
  * two methods with same name different signatures
+ * the compiler chooses which to call based on how you call it
+ * if I call printHW()  I will get the first method
+ * if I call printHW(any int)  I will get the second method
  *  
  * print Hello World on the console 50 times
  * 
@@ -14,12 +17,15 @@
      int count=0;
      while (count < 50) {
         System.out.print("Hello World");
-        count++c
+        count++;
      }
  } // printHW()
  /**
  * this method is overloaded
  * two methods with different signatures
+ * the compiler chooses which to call based on how you call it
+ * if I call printHW()  I will get the first method
+ * if I call printHW(any int)  I will get the second method
  *  
  * print Hello World on the console the number of times in param
  * 
