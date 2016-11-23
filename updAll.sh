ccd28a42797db0831e754cec79877fa5ad4a0ef2
pcampbelldawson <pcampbell.dawson@gmail.com>
2016-11-23 00:21:44 +0000

    MuchBetterCat with constructors

--- updAll.sh
+++ updAll.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # update git repo
+# pmcampbell
+#
 if [[ $# -eq 0 ]] ; then
    echo we need a string for commit message
    exit
 fi
 git add .
-git commit =m "$@"
-git push origin master
+git commit -m "$*"
+if [[ $? -eq 0 ]] ; then
+   git push origin master
+else 
+   echo "git commit error, not pushing"
+fi
