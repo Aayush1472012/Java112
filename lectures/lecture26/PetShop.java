756bbf4e7099075095c78bc30f7cc693b591cfe7
P Campbell <triciamontreal@gmail.com>
2016-11-24 12:30:21 -0500

    create PetShop.java

---
+++ lectures/lecture26/PetShop.java
@@ -0,0 +1,15 @@
+  /**
+ * Class to test ShowDog class 
+ * 420-112-DW
+ * objects with properties & behaviours
+ * 
+ * @author PMC
+ * @version 2015-2016
+ */
+  public class PetShop {
+    public static void main (String[] args)     {
+        ShowDog rover = new ShowDog (35, 	"Labrador", "Fluffy");
+        rover.setName ("Rover");
+        rover.bark ();
+    }  // main()
+}  // PetShop
