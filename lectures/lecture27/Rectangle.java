7422b71d4c994e39cd290e5bb40934ce37bc9b17
P Campbell <triciamontreal@gmail.com>
2016-11-28 17:05:50 -0500

    new file

---
+++ lectures/lecture27/Rectangle.java
@@ -0,0 +1,29 @@
+/**
+ * Metric Logic class  
+ * partly static, like the Math class 
+ * 
+ * @author pmcampbell
+ * @version today
+*/
+
+public class Rectangle { 
+  private static int rectCount = 0'
+  private double length;
+  private double width;
+  /* 
+  * The constructor increments the static field rectCount 
+  * This keeps track of the number of instances of this class that are created
+  */
+  public Rectangle() {
+      // incremented with each new instance
+      rectCount++;
+      }
+  /*
+  * the getRectCount method returns the number of instances of this class
+  * that have been created
+  * @return int value in rectCount field
+  */
+  public static int getRectCount() {
+      return rectCount;
+      }
+ } // Rectangle
